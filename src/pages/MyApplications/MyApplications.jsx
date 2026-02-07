b8417c712dea7dde4f44852e5f6c04f96889ab4a
IRFAN <hrirfan809@gmail.com>
2026-02-08 00:14:02 +0800

    added application functionality

---
+++ src/pages/MyApplications/MyApplications.jsx
@@ -0,0 +1,19 @@
+import React, { Suspense } from 'react';
+import ApplicationStats from './ApplicationStats';
+import ApplicationList from './ApplicationList';
+import useAuth from '../../hooks/useAuth';
+import { myApplicationsPromise } from '../../api/applicationApi';
+
+const MyApplications = () => {
+     const { user } = useAuth();
+    return (
+        <div>
+            <ApplicationStats></ApplicationStats>
+            <Suspense fallback={"Loading applications..."}>
+                <ApplicationList myApplicationsPromise={myApplicationsPromise(user.email)}></ApplicationList>
+            </Suspense>
+        </div>
+    );
+};
+
+export default MyApplications;
